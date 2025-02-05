cbee751d049d401d15251d43a1787bc978535029
Filipe Rosa <filipe331@gmail.com>
2025-02-05 20:19:26 -0300

    feat: Ajusta fluxo token e usuarios de acordo com a role

--- MedicalHealth.Fiap.Aplicacao/Usuario/UsuarioService.cs
+++ MedicalHealth.Fiap.Aplicacao/Usuario/UsuarioService.cs
@@ -1,5 +1,7 @@
 ﻿using MedicalHealth.Fiap.Dominio;
+using MedicalHealth.Fiap.Dominio.Entidades;
 using MedicalHealth.Fiap.Infraestrutura.DTO;
+using MedicalHealth.Fiap.Infraestrutura.Enum;
 using MedicalHealth.Fiap.SharedKernel.MensagensErro;
 using MedicalHealth.Fiap.SharedKernel.Model;
 using static BCrypt.Net.BCrypt;
@@ -15,23 +17,57 @@ namespace MedicalHealth.Fiap.Aplicacao.Usuario
         public async Task<ResponseModel> AutenticarUsuario(AutenticarUsuarioDTO usuarioDTO)
         {
             var validacao = new AutenticarUsuarioDTOValidator().Validate(usuarioDTO);
-            if(!validacao.IsValid)
+            if (!validacao.IsValid)
             {
                 _mensagem = validacao.Errors.Select(x => x.ErrorMessage).ToList();
                 return new ResponseModel(_mensagem, false, null);
             }
 
-            var usuario = await _usuarioRepository.ObterPorEmailAsync(usuarioDTO.Email.ToLower());
-
-            if(usuario is not null)
+            if (usuarioDTO.TipoUsuario == Roles.Administrador)
             {
-                if (Verify(usuarioDTO.Senha, usuario.Senha))
+                var usuario = await _usuarioRepository.ObterUsuarioPorEmailAsync(usuarioDTO.Email.ToLower());
+
+                if (usuario != null && Verify(usuarioDTO.Senha, usuario.Senha))
                 {
                     var token = await _tokenService.ObterToken(usuarioDTO);
 
                     _mensagem.Add(MensagemGenerica.MENSAGEM_SUCESSO);
                     return new ResponseModel(_mensagem, true, token);
                 }
+            }
+            else if (usuarioDTO.TipoUsuario == Roles.Medico)
+            {
+                var medico = await _usuarioRepository.ObterMedicoPorCRMAsync(usuarioDTO.CRM);
+
+                if (medico != null)
+                {
+                    var usuario = await _usuarioRepository.ObterUsuarioPorEmailAsync(medico.Email);
+
+                    if (medico is not null && Verify(usuarioDTO.Senha, usuario.Senha))
+                    {
+                        var token = await _tokenService.ObterToken(usuarioDTO);
+
+                        _mensagem.Add(MensagemGenerica.MENSAGEM_SUCESSO);
+                        return new ResponseModel(_mensagem, true, token);
+                    }
+                }
+            }
+            else if (usuarioDTO.TipoUsuario == Roles.Paciente)
+            {
+                var usuario = await _usuarioRepository.ObterUsuarioPorEmailAsync(usuarioDTO.Email.ToLower());
+
+                if (usuario != null && Verify(usuarioDTO.Senha, usuario.Senha))
+                    {
+                        var token = await _tokenService.ObterToken(usuarioDTO);
+
+                        _mensagem.Add(MensagemGenerica.MENSAGEM_SUCESSO);
+                        return new ResponseModel(_mensagem, true, token);
+                }
+            }
+            else
+            {
+                _mensagem.Add(MensagemUsuario.MENSAGEM_USUARIO_LOGIN_SENHA_INCORRETA);
+                return new ResponseModel(_mensagem, true, null);
             }
 
             _mensagem.Add(MensagemUsuario.MENSAGEM_USUARIO_LOGIN_SENHA_INCORRETA);
